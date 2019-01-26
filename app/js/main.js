6879f6c14368ffcdc585b30a1c48f5ac38e2caff
Kaustubh Mishal <kaustubhm339@gmail.com>
2019-01-26 16:43:14 +0530

    added nav animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
    
+    window.addEventListener("scroll", () => {
+        {
+            // when user scrolls  add animation to navbar
+            let navbar = document.querySelector(".navbar");
+            if (window.scrollY >= 100) {
+                navbar.classList.add("navbarbg");
+                navbar.classList.remove("removeNavbarBg");
+            }
+            else {
+                navbar.classList.remove("navbarbg");
+                navbar.classList.add("removeNavbarBg");
+            }
+        }
+    });
 
 })
